e6dda61ab8a1d2dfc0e99dd978150381f4065e40
Med13Hedi <medhedib@gmail.com>
2025-03-28 01:33:57 +0100

    amazing-animal-paintings: Add cart service, component and module

--- amazing-animal-paintings/src/app/app.module.ts
+++ amazing-animal-paintings/src/app/app.module.ts
@@ -8,19 +8,27 @@ import {HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
+import { CartViewComponent } from './cart/cart-view/cart-view.component';
+import {CartModule} from "./cart/cart.module";
+import {MatListModule} from "@angular/material/list";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ProductModule,
+    CartModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
