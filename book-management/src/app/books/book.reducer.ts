b77ce6c4d3639291c42fca92763db3044a7b24fc
Med13Hedi <medhedib@gmail.com>
2025-03-29 01:51:21 +0100

    book-management: create effects

--- book-management/src/app/books/book.reducer.ts
+++ book-management/src/app/books/book.reducer.ts
@@ -1,5 +1,5 @@
 import {createReducer, on} from "@ngrx/store";
-import {AddBook, RemoveBook} from "./book.actions";
+import {AddBook, AddBookFailure, AddBookSuccess, RemoveBook} from "./book.actions";
 import {Book} from "../models/book";
 
 export const initialState: Book[] = [];
@@ -7,8 +7,19 @@ export const initialState: Book[] = [];
 export const BookReducer = createReducer(
   initialState,
   on(AddBook, // Action
+    (state) => state
+  ),
+  on(AddBookSuccess, // Action
     (state, {id, title, author}) => [...state, {id, title, author}]
   ),
+  on(AddBookFailure, // Action
+    (state, {error}) => {
+      console.error(error);
+      return state;
+    }
+  ),
+
+
   on(RemoveBook, // Action
     (state, {bookId}) => state.filter(book => book.id !== bookId)
   )
