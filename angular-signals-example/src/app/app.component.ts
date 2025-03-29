bfff7933756119809308c1883ae47aea3940fd0b
Med13Hedi <medhedib@gmail.com>
2025-03-29 21:29:33 +0100

    angular-signals-example: setup dark-light mode toggle

---
+++ angular-signals-example/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component, signal, effect} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-signals-example';
+
+  theme = signal('light');
+
+  label = this.theme();
+
+  constructor() {
+
+    effect(() => {
+      this.label = this.theme();
+    })
+  }
+
+
+  toggleDarkMode(){
+    this.theme.update(currentValue => currentValue === 'light' ? 'dark' : 'light');
+  }
+
+}
